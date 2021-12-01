296033dc51e8fb46398d20148e402f3816c5c775
manav <manavghai12octuber@gmail.com>
2021-12-01 11:39:11 +0530

    add input functions

---
+++ inputExtion/input.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string input(string inputStr)
+{
+    string userInputStr;
+    cout << inputStr << endl;
+    cin >> userInputStr;
+    return userInputStr;
+}
