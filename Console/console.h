b094a6b4866ccd313389ed45d9926184a5a72b31
manav <manavghai12octuber@gmail.com>
2021-12-01 12:08:39 +0530

    add bace to Console class

---
+++ Console/console.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+
+class Console
+{
+    private: int func_main(string args[]) { cout << "Hello World" << endl; return 0; }
+};
