2b9129df63b7f58555225f08960484b0e4c0f38d
manav <manavghai12octuber@gmail.com>
2021-12-03 09:28:56 +0530

    add func adding to Console

--- Console/console.h
+++ Console/console.h
@@ -1,11 +1,27 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <functional>
 
 using namespace std;
 
-
 class Console
 {
-    private: int func_main(string args[]) { cout << "Hello World" << endl; return 0; }
+    private:
+        function<int (string[])> func_main = [](string args[]) -> int {
+            cout << "Hello World" << endl;
+            return 0;
+        };
+        void change_FM(function<int (string[])> func) { func_main = func; }
+
+    public:
+        Console(function<int (string[])> func)
+        {
+            change_FM(func);
+        }
 };
+
+int main()
+{
+    return 0;
+}
