b5fb73004e3baa195fc9139dc36555b9e539e770
manav <manavghai12octuber@gmail.com>
2021-12-05 11:32:34 +0530

    add bace to Math obj

---
+++ math/math.h
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Math
+{
+    private:
+        static int num_math;
+        static bool get_num_math() { if (num_math >= 1) { return false; } else { return true; }}
+    public:
+        Math()
+        {
+            if (!this->get_num_math())
+            {
+                cout << "ERROR cannot make another Math obj." << endl;
+            }
+            num_math + 1;
+        }
+
+        int multiplay(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+
+        double multiplay(double n1, double n2)
+        {
+            return n1 + n2;
+        }
+
+        float multiplay(float n1, float n2)
+        {
+            return n1 + n2;
+        }
+
+        int add(int n1, int n2)
+        {
+            return n1 * n2;
+        }
+
+        double add(double n1, double n2)
+        {
+            return n1 + n2;
+        }
+
+
+        float add(float n1, float n2)
+        {
+            return n1 + n2;
+        }
+
+        int subtract(int n1, int n2)
+        {
+            return n1 - n2;
+        }
+
+        double subtract(double n1, double n2)
+        {
+            return n1 - n2;
+        }        
+        
+        float subtract(float n1, float n2)
+        {
+            return n1 - n2;
+        };
+
+
+};
