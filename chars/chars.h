13da4a4412fd9df0d0a8a02fdcce680a27557aa3
manav <manavghai12octuber@gmail.com>
2022-02-03 12:06:53 +0530

    rename CompileChars() -> brew() in chars/chars.h

--- chars/chars.h
+++ chars/chars.h
@@ -32,7 +32,7 @@ string* ConvertChar(char **c, int size)
     return rsarr;
 }
 
-string CompileChar(char c[], int size)
+string brew(char c[], int size)
 {
     string s;
     for (int i {0}; i < size; i++)
