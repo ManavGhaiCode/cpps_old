ae58eeeca875cb1520bcc52a02b70ce85f8b771e
manav <manavghai12octuber@gmail.com>
2022-02-14 09:55:56 +0530

    use vars.h to console.h

--- Console/console.h
+++ Console/console.h
@@ -6,37 +6,45 @@
 #include <string>
 #include <functional>
 
+#include "../vars.h"
+
 using namespace std;
 
 class Console
 {
-    private:
-        function<int (string[])> func_main = [](string args[]) -> int {
-            cout << "Hello World" << endl;
-            return 0;
-        };
-        void change_FM(function<int (string[])> func) { func_main = func; }
+private:
+    function<int(string[])> func_main = [](string args[]) -> int
+    {
+        cout << HW << endl;
+        return 0;
+    };
+    void change_FM(function<int(string[])> func) { func_main = func; }
 
-    public:
+public:
+    Console()
+    {
+        this->run();
+    }
 
-        Console()
-        {
-            this->run();
-        }
+    Console(function<int(string[])> func)
+    {
+        change_FM(func);
+        this->run();
+    }
 
-        Console(function<int (string[])> func)
-        {
-            change_FM(func);
-            this->run();
-        }
+    void run()
+    {
+        cout << "Console - "
+             << '\n'
+             << endl;
+        string arr[3];
+        arr[0] = BrewLOL('\0');
+        arr[1] = BrewLOL('2');
+        arr[2] = BrewLOL('3');
 
-        void run()
-        {
-            cout << "Console - " << "\n" << endl;
-            string arr[] {"lol", "lol2", "lol3"};
-            func_main(arr);
-            cout << endl;
-        }
+        func_main(arr);
+        cout << endl;
+    }
 };
 
 int main()
