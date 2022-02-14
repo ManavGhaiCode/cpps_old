10a1204b3f3dc2f3b7750d15b29e35a61a07b580
manav <manavghai12octuber@gmail.com>
2022-02-14 09:37:05 +0530

    re-add 'Hello World' to vars.h

--- vars.h
+++ vars.h
@@ -8,30 +8,32 @@
 
 // ERROR array / ERROR string
 
-char ArrBrewERROR[6] {'E', 'R', 'R', 'O', 'R', ':'};
+char ArrBrewERROR[6]{'E', 'R', 'R', 'O', 'R', ':'};
 string ERROR = brew(ArrBrewERROR, 6);
 
 // ERROR brew func
 
-string BrewERROR(char var[], int size) {
+string BrewERROR(char var[], int size)
+{
     return ERROR + brew(var, size);
 }
 
 // "()" arrays and brew
 
-char ArrBrew9[1] {'('};
-char ArrBrew0[1] {')'};
+char ArrBrew9[1]{'('};
+char ArrBrew0[1]{')'};
 string Brew9 = brew(ArrBrew9, 1);
 string Brew0 = brew(ArrBrew0, 1);
 
 // add " " str brew
 
-char sstr[1] {' '};
+char sstr[1]{' '};
 string BrewSStr = brew(sstr, 1);
 
 // empty str function
 
-string estr() {
+string estr()
+{
     string s = '\0';
 
     return s;
@@ -39,18 +41,19 @@ string estr() {
 
 // lol's
 
-char ArrBrewLOL[3] {'l', 'o', 'l'};
+char ArrBrewLOL[3]{'l', 'o', 'l'};
 string lol = brew(ArrBrewLOL, 3);
 
 // A function to brew LOL
 
-string BrewLOL(char var) {
+string BrewLOL(char var)
+{
     return lol + var;
 }
 
 // Hellow World function
 
-char ArrBrewHW[11] {'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd'};
-string HW = brew(ArrBrewHW);
+char ArrBrewHW[11]{'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd'};
+string HW = brew(ArrBrewHW, 11);
 
 #endif
