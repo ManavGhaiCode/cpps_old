50db81213571854af4bc547eaf8ff0b88ad219d7
manav <manavghai12octuber@gmail.com>
2022-02-15 20:55:33 +0530

    bake vars.h for chars.h / use vars in chars.h

--- chars/chars.h
+++ chars/chars.h
@@ -6,24 +6,39 @@
 
 using namespace std;
 
+string brew(char c[], int size)
+{
+    string s;
+    for (int i{0}; i < size; i++)
+    {
+        s = s + c[i];
+    }
+}
+
+char ArrEmptyStr[1]{'\0'};
+string emptyStr = brew(ArrEmptyStr, 1);
+
+char space[1]{' '};
+string spaceStr = brew(space, 1);
+
 string *ConvertChar(char **c, int size)
 {
     if (size <= 100)
     {
-        string rsarr2[1]{""};
+        string rsarr2[1]{emptyStr};
         std::cout << "Can not take more than 99 chars." << std::endl;
         return rsarr2;
     }
 
     int j;
-    string rsarr[100]{""};
+    string rsarr[100]{emptyStr};
 
     for (int i{0}; i < size; i++)
     {
-        if (c[i] == " ")
+        if (c[i] == spaceStr)
         {
             j++;
-            rsarr[j] = "";
+            rsarr[j] = emptyStr;
         }
         else
         {
@@ -32,15 +47,6 @@ string *ConvertChar(char **c, int size)
     }
 
     return rsarr;
-}
-
-string brew(char c[], int size)
-{
-    string s;
-    for (int i{0}; i < size; i++)
-    {
-        s = s + c[i];
-    }
 }
 
 #endif
