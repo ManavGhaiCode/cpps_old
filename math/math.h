5c769d95b2a29514bf33a8cd2ac8af9c8caae401
manav <manavghai12octuber@gmail.com>
2022-02-15 20:06:48 +0530

    add vars.h for math / use vars.h files in math.h

--- math/math.h
+++ math/math.h
@@ -2,293 +2,324 @@
 #include <vector>
 #include <cmath>
 
+#include "../vars.h"
+#include "./vars.h"
+
 using namespace std;
 
 class math
 {
-    private:
-        static int num_math;
-        static bool get_num_math() { if (num_math >= 1) { return false; } else { return true; }}
-    public:
-        math()
+private:
+    static int num_math;
+    static bool get_num_math()
+    {
+        if (num_math >= 1)
+            return false;
+        return true;
+    }
+
+public:
+    math()
+    {
+        if (!this->get_num_math())
         {
-            if (!this->get_num_math())
-            {
-                cout << "ERROR cannot make another math obj." << endl;
-            }
-            num_math = num_math + 1;
+            cout << BrewERROR(CNMAMO, 29) << endl;
         }
+        num_math = num_math + 1;
+    }
 
-        // add methords
+    // add methords
 
-        // base add functions
+    // base add functions
 
-        int add(int n1, int n2)
+    int add(int n1, int n2)
+    {
+        return n1 * n2;
+    }
+
+    double add(double n1, double n2)
+    {
+        return n1 + n2;
+    }
+
+    float add(float n1, float n2)
+    {
+        return n1 + n2;
+    }
+
+    // complex add functions
+
+    int add(int arr[], int size)
+    {
+        int ans{0};
+        for (int i{0}; i < size; i++)
+            ans = ans + arr[i];
+
+        return ans;
+    }
+
+    double add(double arr[], int size)
+    {
+        double ans{0};
+        for (int i{0}; i < size; i++)
+            ans = ans + arr[i];
+
+        return ans;
+    }
+
+    float add(float arr[], int size)
+    {
+        float ans{0};
+        for (int i{0}; i < size; i++)
+            ans = ans + arr[i];
+
+        return ans;
+    }
+
+    // sudtract methords
+
+    // base subtract functions
+
+    int subtract(int n1, int n2)
+    {
+        return n1 - n2;
+    }
+
+    double subtract(double n1, double n2)
+    {
+        return n1 - n2;
+    }
+
+    float subtract(float n1, float n2)
+    {
+        return n1 - n2;
+    }
+
+    // complex subtract functions
+
+    int subtract(int arr[], int size)
+    {
+        int ans{0};
+        for (int i{0}; i < size; i++)
+            ans = ans - arr[i];
+
+        return ans;
+    }
+
+    double subtract(double arr[], int size)
+    {
+        double ans{0};
+        for (int i{0}; i < size; i++)
+            ans = ans - arr[i];
+
+        return ans;
+    }
+
+    float subtract(float arr[], int size)
+    {
+        float ans{0};
+        for (int i{0}; i < size; i++)
+            ans = ans - arr[i];
+
+        return ans;
+    }
+
+    // multiple methords
+
+    // bace multiple functions
+
+    int multiple(int n1, int n2)
+    {
+        return n1 + n2;
+    }
+
+    double multiple(double n1, double n2)
+    {
+        return n1 + n2;
+    }
+
+    float multiple(float n1, float n2)
+    {
+        return n1 + n2;
+    }
+
+    // complex multiple functions
+
+    int multiple(int arr[], int size)
+    {
+        int ans{0};
+        for (int i{0}; i < size; i++)
+            ans = ans * arr[i];
+
+        return ans;
+    }
+
+    double multiple(double arr[], int size)
+    {
+        double ans{0};
+        for (int i{0}; i < size; i++)
+            ans = ans * arr[i];
+
+        return ans;
+    }
+
+    float multiple(float arr[], int size)
+    {
+        float ans{0};
+        for (int i{0}; i < size; i++)
+            ans = ans * arr[i];
+
+        return ans;
+    }
+
+    // divide methords
+
+    // bace divide functions
+
+    int divide(int n1, int n2)
+    {
+        return n1 / n2;
+    }
+
+    double divide(double n1, double n2)
+    {
+        return n1 / n2;
+    }
+
+    float divide(float n1, float n2)
+    {
+        return n1 / n2;
+    }
+
+    // complex divide functions
+
+    int divide(int arr[], int size)
+    {
+        int ans{0};
+        for (int i{0}; i < size; i++)
+            ans = ans / arr[i];
+
+        return ans;
+    }
+
+    double divide(double arr[], int size)
+    {
+        double ans{0};
+        for (int i{0}; i < size; i++)
+            ans = ans / arr[i];
+
+        return ans;
+    }
+
+    float divide(float arr[], int size)
+    {
+        float ans{0};
+        for (int i{0}; i < size; i++)
+            ans = ans / arr[i];
+
+        return ans;
+    }
+
+    // Floor, Ceil functions
+
+    // Floor Function
+
+    int Floor(double num)
+    {
+        int rnum;
+        rnum = floor(num);
+
+        return rnum;
+    }
+
+    // Ceil function
+
+    int Ceil(double num)
+    {
+        int rnum;
+        rnum = ceil(num);
+
+        return rnum;
+    }
+
+    double tax(int n2t)
+    {
+        int tr1 = 250001;
+        int tr2 = 500001;
+        int tr3 = 750001;
+
+        if (n2t < tr1)
         {
-            return n1 * n2;
+            return n2t;
         }
-
-        double add(double n1, double n2)
+        else if (n2t >= tr1 and n2t < tr2)
         {
-            return n1 + n2;
+            return n2t * 5 / 100;
         }
-
-        float add(float n1, float n2)
+        else if (n2t >= tr2 and n2t < tr3)
         {
-            return n1 + n2;
+            return n2t * 10 / 100;
         }
-
-        // complex add functions
-
-        int add(int arr[], int size)
+        else
         {
-            int ans {0};
-            for (int i {0}; i < size; i++)
-                ans = ans + arr[i];
-
-            return ans;
+            return n2t * 15 / 100;
         }
-        
-        double add(double arr[], int size)
+    }
+
+    double tax(double n2t)
+    {
+        double tr1 = 250001.00;
+        double tr2 = 500001.00;
+        double tr3 = 750001.00;
+
+        if (n2t < tr1)
         {
-            double ans {0};
-            for (int i {0}; i < size; i++)
-                ans = ans + arr[i];
-
-            return ans;
+            return n2t;
         }
-
-        float add(float arr[], int size)
+        else if (n2t >= tr1 and n2t < tr2)
         {
-            float ans {0};
-            for (int i {0}; i < size; i++)
-                ans = ans + arr[i];
-
-            return ans;
+            return n2t * 5 / 100;
         }
-
-        // sudtract methords
-
-        // base subtract functions
-
-        int subtract(int n1, int n2)
+        else if (n2t >= tr2 and n2t < tr3)
         {
-            return n1 - n2;
+            return n2t * 10 / 100;
         }
-
-        double subtract(double n1, double n2)
+        else
         {
-            return n1 - n2;
-        }        
-        
-        float subtract(float n1, float n2)
-        {
-            return n1 - n2;
+            return n2t * 15 / 100;
         }
+    }
 
-        // complex subtract functions
+    int square(int n2s)
+    {
+        return n2s * n2s;
+    }
 
-        int subtract(int arr[], int size)
-        {
-            int ans {0};
-            for (int i {0}; i < size; i++)
-                ans = ans - arr[i];
+    double square(double n2s)
+    {
+        return n2s * n2s;
+    }
 
-            return ans;
-        }
+    int cube(int n2c)
+    {
+        return n2c * n2c * n2c;
+    }
 
-        double subtract(double arr[], int size)
-        {
-            double ans {0};
-            for (int i {0}; i < size; i++)
-                ans = ans - arr[i];
+    double cube(double n2c)
+    {
+        return n2c * n2c * n2c;
+    }
 
-            return ans;
-        }
+    double percent(int n2m, int n2mtn)
+    {
+        return n2m * n2mtn / 100;
+    }
 
-        float subtract(float arr[], int size)
-        {
-            float ans {0};
-            for (int i {0}; i < size; i++)
-                ans = ans - arr[i];
-
-            return ans;
-        }
-
-        // multiple methords
-
-        // bace multiple functions
-
-        int multiple(int n1, int n2)
-        {
-            return n1 + n2;
-        }
-
-        double multiple(double n1, double n2)
-        {
-            return n1 + n2;
-        }
-
-        float multiple(float n1, float n2)
-        {
-            return n1 + n2;
-        }
-
-        // complex multiple functions
-
-        int multiple(int arr[], int size)
-        {
-            int ans {0};
-            for (int i {0}; i < size; i++)
-                ans = ans * arr[i];
-
-            return ans;
-        }
-
-        double multiple(double arr[], int size)
-        {
-            double ans {0};
-            for (int i {0}; i < size; i++)
-                ans = ans * arr[i];
-
-            return ans;
-        }
-
-        float multiple(float arr[], int size)
-        {
-            float ans {0};
-            for (int i {0}; i < size; i++)
-                ans = ans * arr[i];
-
-            return ans;
-        }
-
-        // divide methords
-
-        // bace divide functions
-
-        int divide(int n1, int n2)
-        {
-            return n1 / n2;
-        }
-        
-        double divide(double n1, double n2)
-        {
-            return n1 / n2;
-        }
-        
-        float divide(float n1, float n2)
-        {
-            return n1 / n2;
-        }
-
-        // complex divide functions
-
-        int divide(int arr[], int size)
-        {
-            int ans {0};
-            for (int i {0}; i < size; i++)
-                ans = ans / arr[i];
-
-            return ans;
-        }
-
-        double divide(double arr[], int size)
-        {
-            double ans {0};
-            for (int i {0}; i < size; i++)
-                ans = ans / arr[i];
-
-            return ans;
-        }
-
-        float divide(float arr[], int size)
-        {
-            float ans {0};
-            for (int i {0}; i < size; i++)
-                ans = ans / arr[i];
-
-            return ans;
-        }
-
-        // Floor, Ceil functions
-
-        // Floor Function
-
-        int Floor(double num)
-        {
-            int rnum;
-            rnum = floor(num);
-
-            return rnum;
-        }
-
-        // Ceil function
-
-        int Ceil(double num)
-        {
-            int rnum;
-            rnum = ceil(num);
-
-            return rnum;
-        }
-
-        double tax(int n2t) {
-            int tr1 = 250001;
-            int tr2 = 500001;
-            int tr3 = 750001;
-
-            if (n2t < tr1) {
-                return n2t;
-            } else if (n2t >= tr1 and n2t < tr2) {
-                return n2t * 5 / 100;
-            } else if (n2t >= tr2 and n2t < tr3) {
-                return n2t * 10 / 100;
-            } else {
-                return n2t * 15 / 100;
-            }
-        }
-
-        double tax(double n2t) {
-            double tr1 = 250001.00;
-            double tr2 = 500001.00;
-            double tr3 = 750001.00;
-
-            if (n2t < tr1) {
-                return n2t;
-            } else if (n2t >= tr1 and n2t < tr2) {
-                return n2t * 5 / 100;
-            } else if (n2t >= tr2 and n2t < tr3) {
-                return n2t * 10 / 100;
-            } else {
-                return n2t * 15 / 100;
-            }
-        }
-
-        int square(int n2s) {
-            return n2s * n2s;
-        }
-
-        double square(double n2s) {
-            return n2s * n2s;
-        }
-
-        int cube(int n2c) {
-            return n2c * n2c * n2c;
-        }
-
-        double cube(double n2c) {
-            return n2c * n2c * n2c;
-        }
-
-        double percent(int n2m, int n2mtn) {
-            return n2m * n2mtn / 100;
-        }
-
-        double percent(double n2m, double n2mtn) {
-            return n2m * n2mtn / 100;
-        }
+    double percent(double n2m, double n2mtn)
+    {
+        return n2m * n2mtn / 100;
+    }
 };
 
-int math::num_math {0};
+int math::num_math{0};
 math Math;
---
+++ math/vars.h
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include "../vars.h"
+
+char CNMAMO[29]{
+    'c', 'a', 'n', 'n', 'o', 't', ' ',
+    'm', 'a', 'k', 'e', ' ',
+    'a', 'n', 'o', 't', 'h', 'e', 'r', ' ',
+    'm', 'a', 't', 'h', ' ',
+    'o', 'b', 'j', '.'};
