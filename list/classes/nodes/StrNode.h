15344a84edc2b8e31823568bfff2b0868ee9d92f
manav <manavghai12octuber@gmail.com>
2022-04-07 11:19:46 +0530

    add len to strNode

--- list/classes/nodes/StrNode.h
+++ list/classes/nodes/StrNode.h
@@ -7,12 +7,19 @@ class strNode : public baseNode
 {
 private:
     std::string value;
+    int len;
 
 public:
     strNode(std::string val, int i)
     {
         this->value = val;
         this->index = i;
+        this->len = val.size();
+    }
+
+    int getLen()
+    {
+        return this->len;
     }
 
     virtual ~strNode(){};
