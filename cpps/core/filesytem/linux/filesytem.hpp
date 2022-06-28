66ba1d1f3d05d6d75d2e83d137fc555cce320e9d
ManavGhaiCode <manavghai12october@gmail.com>
2022-06-28 08:58:21 +0530

    add base to function fileSystem::ls() to linux branch

--- cpps/core/filesytem/linux/filesytem.hpp
+++ cpps/core/filesytem/linux/filesytem.hpp
@@ -1,1 +1,26 @@
-namespace fileSystem {};
+#include <iostream>
+#include <vector>
+#include <dirent.h>
+#include <filesystem>
+
+namespace fileSystem {
+    std::vector<std::string> ls() {
+        std::vector<std::string> returnValue;
+
+        DIR *dir;
+        struct dirent *ent;
+
+        if ((dir = opendir("/")) != NULL) {
+            while ((ent = readdir(dir)) != NULL) {
+                returnValue.push_back(ent->d_name);
+            }
+
+            closedir(dir);
+
+            return returnValue;
+        } else {
+            return returnValue;
+        }
+    }
+
+};
