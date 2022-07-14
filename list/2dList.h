a1e53635e9ca88ec3d239fa09fe692fc2c8df769
ManavGhaiCode <manavghai12october@gmail.com>
2022-07-14 09:55:07 +0530

    refactored dir and files for class lists2D

--- list/2dList.h
+++ list/2dList.h
@@ -1,6 +1,6 @@
 #ifndef _2D_LIST_H_
 #define _2D_LIST_H_
 
-#include "./2dList/2dListIF.h"
+#include "./2dList/2dList.h"
 
 #endif
--- list/2dList/2dListIF.h
+++
@@ -1,6 +0,0 @@
-#ifndef _2D_LIST_IF_H_
-#define _2D_LIST_IF_H_
-
-#include "./classes/2dListClass.h"
-
-#endif
