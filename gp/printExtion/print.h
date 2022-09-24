20e00718a7f6bfbeed9a3857f76c1a21ba71178f
ManavGhaiCode <manavghai@gmail.com>
2022-09-24 13:50:35 +0530

    redo print(); function

--- gp/printExtion/print.h
+++ gp/printExtion/print.h
@@ -2,246 +2,50 @@
 #define _PRINT_H_
 
 #include <iostream>
+#include <functional>
 #include <string>
 #include <vector>
 
 #include "../../vars.h"
 
-using std::cout;
-using std::endl;
-using std::string;
-using std::vector;
-
-void print(int var)
-{
-    cout << var << endl;
+template<typename T> void print(T obj) {
+    std::cout << obj << std::endl;
 }
 
-void print(double var)
-{
-    cout << var << endl;
+template<typename T> void print(T obj, std::function<void(T)> func) {
+    func(obj);
 }
 
-void print(string var)
-{
-    cout << var << endl;
-}
-
-void print(char var)
-{
-    cout << var << endl;
-}
-
-void print(int var[])
-{
-    cout << var << endl;
-}
-
-void print(double var[])
-{
-    cout << var << endl;
-}
-
-void print(string var[])
-{
-    cout << var << endl;
-}
-
-void print(char var[])
-{
-    cout << var << endl;
-}
-
-void print(vector<int> var)
-{
-    cout << "[ ";
-
-    for (auto s : var)
-        cout << s << ", ";
-
-    cout << "]" << endl;
-}
-
-void print(vector<double> var)
-{
-    cout << "[ ";
-
-    for (auto s : var)
-        cout << s << ", ";
-
-    cout << "]" << endl;
-}
-
-void print(vector<string> var)
-{
-    cout << "[ ";
-
-    for (auto s : var)
-        cout << s << ", ";
-
-    cout << "]" << endl;
-}
-
-void print(vector<char> var)
-{
-    cout << "[ ";
-
-    for (auto s : var)
-        cout << s << ", ";
-
-    cout << "]" << endl;
-}
-
-void print(int var, bool opt)
-{
-    if (opt == true)
-        cout << var << endl;
-    else
-        cout << var;
-}
-
-void print(double var, bool opt)
-{
-    if (opt == true)
-        cout << var << endl;
-    else
-        cout << var;
-}
-
-void print(string var, bool opt)
-{
-    if (opt == true)
-        cout << var << endl;
-    else
-        cout << var;
-}
-
-void print(char var, bool opt)
-{
-    if (opt == true)
-        cout << var << endl;
-    else
-        cout << var;
-}
-
-void print(int var[], bool opt)
-{
-    if (opt == true)
-        cout << var << endl;
-    else
-        cout << var;
-}
-
-void print(double var[], bool opt)
-{
-    if (opt == true)
-        cout << var << endl;
-    else
-        cout << var;
-}
-
-void print(string var[], bool opt)
-{
-    if (opt == true)
-        cout << var << endl;
-    else
-        cout << var;
-}
-
-void print(char var[], bool opt)
-{
-    if (opt == true)
-        cout << var << endl;
-    else
-        cout << var;
-}
-
-void print(vector<int> var, bool opt)
-{
-    if (opt == true)
-    {
-        cout << "[ ";
-
-        for (auto s : var)
-            cout << s << ", ";
-
-        cout << "]" << endl;
+template<typename T> void print(std::vector<T> obj) {
+    std::cout << "[ ";
+    for (T childObj : obj) {
+        std::cout << childObj << ", ";
     }
-    else
-    {
-        cout << "[ ";
-
-        for (auto s : var)
-            cout << s << ", ";
-
-        cout << "]";
-    }
+    std::cout << " ]" << std::endl;
 }
 
-void print(vector<double> var, bool opt)
-{
-    if (opt == true)
-    {
-        cout << "[ ";
-
-        for (auto s : var)
-            cout << s << ", ";
-
-        cout << "]" << endl;
+template<typename T> void print(std::vector<T> obj, std::function<void(T)> func) {
+    std::cout << "[ ";
+    for (T childObj : obj) {
+        func(childObj);
     }
-    else
-    {
-        cout << "[ ";
-
-        for (auto s : var)
-            cout << s << ", ";
-
-        cout << "]";
-    }
+    std::cout << " ]" << std::endl;
 }
 
-void print(vector<string> var, bool opt)
-{
-    if (opt == true)
-    {
-        cout << "[ ";
-
-        for (auto s : var)
-            cout << s << ", ";
-
-        cout << "]" << endl;
+template<typename T> void print(T obj[], int size) {
+    std::cout << "[ ";
+    for (int i = 0; i < size; i++) {
+        std::cout << obj[i] << ", ";
     }
-    else
-    {
-        cout << "[ ";
-
-        for (auto s : var)
-            cout << s << ", ";
-
-        cout << "]";
-    }
+    std::cout << "]" << std::endl;
 }
 
-void print(vector<char> var, bool opt)
-{
-    if (opt == true)
-    {
-        cout << "[ ";
-
-        for (auto s : var)
-            cout << s << ", ";
-
-        cout << "]" << endl;
+template<typename T> void print(T obj[], int size, std::function<void(T)> func) {
+    std::cout << "[ ";
+    for (int i = 0; i < size; i++) {
+        func(obj[i]);
     }
-    else
-    {
-        cout << "[ ";
-
-        for (auto s : var)
-            cout << s << ", ";
-
-        cout << "]";
-    }
+    std::cout << " ]" << std::endl;
 }
 
 #endif
