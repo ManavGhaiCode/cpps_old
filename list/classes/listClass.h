8e6b529850510a0ff0100ba4d4a5a54c855ab575
ManavGhaiCode <manavghai@gmail.com>
2023-01-14 09:46:44 +0530

    add function push(); to class List

--- list/classes/listClass.h
+++ list/classes/listClass.h
@@ -10,9 +10,24 @@ class List
         std::vector<DoubleNode> vect_Doubles;
         std::vector<StringNode> vect_Strings;
 
+        int len = 0;
+        int indexes = -1;
+
     public:
         List() {};
-
         ~List() {};
+
+        void push(int raw_value) {
+            IntNode value;
+
+            value.value = raw_value;
+            value.index = len;
+            value.vect_index = vect_Ints.size();
+
+            vect_Ints.push_back( value );
+
+            len += 1;
+            indexes += 1;
+        }
         
 };
